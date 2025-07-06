ab55d7b82647dc9d15a129507dbd29981611a8d1
Martina2612 <martiabril2612@gmail.com>
2025-07-06 00:51:31 -0300

    s

--- recetasapp/src/main/java/com/recetas/recetasapp/controller/UsuarioController.java
+++ recetasapp/src/main/java/com/recetas/recetasapp/controller/UsuarioController.java
@@ -178,7 +178,7 @@ public class UsuarioController {
             Files.copy(file.getInputStream(), filePath);
             
             // Actualizar URL en base de datos
-            String fotoUrl = "/uploads/profile_photos/" + fileName;
+            String fotoUrl = "/api/imagenes/perfil/" + fileName;
             usuarioService.actualizarFotoPerfil(userId, fotoUrl);
             
             return ResponseEntity.ok(Map.of("fotoUrl", fotoUrl));

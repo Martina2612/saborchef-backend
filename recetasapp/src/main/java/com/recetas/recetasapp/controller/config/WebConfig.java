dcbe137c7234d20f13c025b7ea1a7dce8d46fe39
Martina2612 <martiabril2612@gmail.com>
2025-07-06 00:43:40 -0300

    s

--- recetasapp/src/main/java/com/recetas/recetasapp/controller/config/WebConfig.java
+++ recetasapp/src/main/java/com/recetas/recetasapp/controller/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/profile_photos/**")
+                .addResourceLocations("file:uploads/profile_photos/");
     }
 }
